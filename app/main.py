4b8d42088bf62faed8c4241af45d2f9e8cda0a6b
Juan-789 <juan.marulanda@stu.ocsb.ca>
2024-01-05 01:16:28 -0500

    made token authentication, and user login authentication

--- app/main.py
+++ app/main.py
@@ -10,7 +10,7 @@ import time
 from sqlalchemy.orm import Session
 from . import models, schemas, utils
 from .database import engine, get_db
-from .routers import post, user
+from .routers import post, user, auth
 
 
 
@@ -49,6 +49,8 @@ def find_index(id):
 
 app.include_router(post.router)
 app.include_router(user.router)
+app.include_router(auth.router)
+
 
 @app.get("/")
 async def root():
